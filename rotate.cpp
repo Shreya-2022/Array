6c15f3c1e1797aad5d9099fb847ac6aa655219b9
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-09-08 15:55:32 +0530

    Rotate array

---
+++ rotate.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main(){
+
+    int array[]={1,2,3,4,5};
+    int n = 5;
+    int k =2;
+    k=k%n;
+    int j=0;
+
+    int ansarray[5];
+    for(int i=n-k;i<n;i++){
+        ansarray[j++]=array[i];
+        
+    }
+    for(int i=0;i<=k;i++){
+        ansarray[j++]=array[i];
+
+    }
+    for(int i=0;i<n;i++){
+        cout<<ansarray[i]<<" ";
+
+    }
+    cout<<endl;
+    return 0;
+}

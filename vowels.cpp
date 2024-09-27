cb6816173299ade9cd5d882b076e6ef877cb894b
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-09-27 21:07:08 +0530

    vowels

---
+++ vowels.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    if(ch=='a'){
+        cout<<"vowel"<<endl;
+    }
+    else if(ch=='e'){
+        cout<<"vowel"<<endl;
+    }
+    else if(ch=='i'){
+        cout<<"vowel"<<endl;
+    }
+    else if(ch=='o'){
+        cout<<"vowel"<<endl;
+    }
+    else if(ch=='u'){
+        cout<<"vowel"<<endl;
+        }
+        else{
+            cout<<"Not vowels"<<endl;
+
+        }
+
+        return 0;
+
+
+        
+}
